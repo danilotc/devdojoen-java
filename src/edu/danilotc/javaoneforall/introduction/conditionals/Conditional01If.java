087b5ecf58b8b2d4f6a7979a81b8bf3a0a616fa7
danilotc <danilotrindade87@gmail.com>
2023-06-16 12:35:26 -0300

    Class 20 - Conditional Statement pt 01 - IF

---
+++ src/edu/danilotc/javaoneforall/introduction/conditionals/Conditional01If.java
@@ -0,0 +1,23 @@
+package edu.danilotc.javaoneforall.introduction.conditionals;
+
+public class Conditional01If {
+    public static void main(String[] args) {
+        int age = 15;
+        boolean isAllowedToBuyAlcohol = age >= 18;
+
+        if(isAllowedToBuyAlcohol){
+            System.out.println("Authorized to buy alcohol");
+        }
+
+        if(!isAllowedToBuyAlcohol){
+            System.out.println("Sorry, you are not allowed to buy alcohol");
+        }
+
+        boolean c = false;
+        if(c = false){
+            System.out.println("Inside weird condition");
+        }
+
+        System.out.println("Outside IF");
+    }
+}
