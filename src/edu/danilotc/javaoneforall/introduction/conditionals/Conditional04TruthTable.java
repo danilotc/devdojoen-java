bb62f8a5364ddef8373cd4f9a5b2b6fe80a3bc92
danilotc <danilotrindade87@gmail.com>
2023-06-17 10:38:20 -0300

    Class 23 - Conditional Statement pt 04 - Truth Table

---
+++ src/edu/danilotc/javaoneforall/introduction/conditionals/Conditional04TruthTable.java
@@ -0,0 +1,27 @@
+package edu.danilotc.javaoneforall.introduction.conditionals;
+
+public class Conditional04TruthTable {
+    public static void main(String[] args) {
+        //             &&
+        //  -------------------------
+        //  T && T = T
+        //  T && F = F
+        //  F && T = F
+        //  F && F = F
+        //  T && T && T && T && F = F
+        //
+        //             ||
+        //  -------------------------
+        //  T || T = T
+        //  T || F = T
+        //  F || T = T
+        //  F || F = F
+        //  F || F || F || F || T = T
+        //
+        //  I want to know how much I have to pay in the Netherlands 2020
+        //  based on my annual salary
+
+        //  More specific information to take the exercise:
+        //  https://expatax.nl/knowledge-base/tax-rates-in-the-netherlands-2020
+    }
+}
