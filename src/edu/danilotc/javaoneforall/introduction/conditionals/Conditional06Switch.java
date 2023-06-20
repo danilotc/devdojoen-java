ce75c46328a7db2ba60e51c684557044e309eaef
danilotc <danilotrindade87@gmail.com>
2023-06-20 00:09:21 -0300

    Class 25 - Conditional Statement pt 06 - Switch

---
+++ src/edu/danilotc/javaoneforall/introduction/conditionals/Conditional06Switch.java
@@ -0,0 +1,51 @@
+package edu.danilotc.javaoneforall.introduction.conditionals;
+
+public class Conditional06Switch {
+    public static void main(String[] args) {
+        // Acceptable types: char, int, byte, short, enum, String
+        byte day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Sanday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid");
+                break;
+
+        }
+
+        char gender = 'F';
+        switch (gender) {
+            case 'M': {
+                System.out.println("Male");
+                break;
+            }
+            case 'F': {
+                System.out.println("Female");
+                break;
+            }
+            default: {
+                System.out.println("Invalid option");
+                break;
+            }
+        }
+    }
+}
