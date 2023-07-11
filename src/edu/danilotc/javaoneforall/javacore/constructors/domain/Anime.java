6c88389daefd2ac1ae3863edac1a6c588fe5a9e0
danilotc <danilotrindade87@gmail.com>
2023-07-11 13:05:30 -0300

    Class 59 - Object Orientation - Constructors pt 02 - Overloading

--- src/edu/danilotc/javaoneforall/javacore/constructors/domain/Anime.java
+++ src/edu/danilotc/javaoneforall/javacore/constructors/domain/Anime.java
@@ -5,6 +5,7 @@ public class Anime {
     private String type;
     private int episodes;
     private String status;
+    private String studio;
 
     // Empty constructor or default constructor
     public Anime() {
@@ -19,11 +20,18 @@ public class Anime {
         this.status = status;
     }
 
+    // Overloading constructor
+    public Anime(String name, String type, int episodes, String status, String studio) {
+        this(name, type, episodes, status);
+        this.studio = studio;
+    }
+
     public void print() {
         System.out.println(this.name);
         System.out.println(this.type);
         System.out.println(this.episodes);
         System.out.println(this.status);
+        System.out.println(this.studio);
     }
 
     public String getName() {
@@ -56,5 +64,13 @@ public class Anime {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
     }
 }
--- src/edu/danilotc/javaoneforall/javacore/constructors/test/AnimeTest.java
+++ src/edu/danilotc/javaoneforall/javacore/constructors/test/AnimeTest.java
@@ -4,7 +4,7 @@ import edu.danilotc.javaoneforall.javacore.constructors.domain.Anime;
 
 public class AnimeTest {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyuu", "TV", 12, "Finishing");
+        Anime anime = new Anime("Haikyuu", "TV", 12, "Finishing", "Production IG");
         anime.print();
     }
 }
